21a8eed7a2b82447b0eed0b950152e8d4828b8d3
Felix_Desktop <dede6487@gmail.com>
2022-11-08 18:40:01 +0100

    starting doku

--- ProgAufg1/CompR.m
+++
@@ -1,6 +0,0 @@
-function [R] = CompR(B,D, p , k )
-
-R= triu ( B (: , p ));
-R= full ( spdiags (D ,0 , R) );
-
-end
--- ProgAufg1/EigenWert.m
+++ ProgAufg1/EigenWert.m
@@ -1,14 +1,32 @@
-function [lambda, x, exitflag] = EigenWert(A, mu, max_iter)
+function[lambda, x, exitflag] = EigenWert(A, mu, max_iter)
 
-if nargin < 3
-    max_iter = 500;
+    if nargin < 3
+        max_iter = 500;
+    end
 
-exit_flag = 1;
+    exitflag = 1;
 
-[m,n] = size(A);
+    [m, n] = size(A);
+    if m ~= n
+        disp('Matrix ist nicht quadratisch.');
+        return;
+    end
 
-if m ~= n
-    disp("")
+    z = ones(n, 1);
+    I = eye(n);
+    for k = 1 : max_iter
+        v = z/norm(z, 2);
+        z = linsolve((A-mu*I), v);
+        v = z/norm(z, 2);
+    end
 
+    %exitcondition needs to be checked
 
+    lambda = v'*(A*v);
+
+    if norm(A*v - lambda*v) < 1e-10
+        exitflag = 0;
+    end
+
+    x = v;
 end
