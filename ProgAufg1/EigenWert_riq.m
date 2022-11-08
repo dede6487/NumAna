04f40b13a6ca757575c070a48da26e6ec78d1555
Felix <dede4861@gmail.com>
2022-11-08 10:50:03 +0100

    nargin

---
+++ ProgAufg1/EigenWert_riq.m
@@ -0,0 +1,27 @@
+function[lambda, x, exitflag] = EigenWert(A, mu, max_iter)
+
+    if nargin < 3
+        max_iter = 500;
+    end
+
+    exit_flag = 1;
+
+    [m, n] = size(A);
+    if m ~= n
+        disp('Matrix ist nicht quadratisch.');
+        return;
+    end
+
+    z = ones(n, 1);
+    I = eye(n);
+    for k = 1 : max_iter
+        v = z/norm(z, 2);
+        z = linsolve((A-mu*I), v);
+        v = z/norm(z, 2);
+    end
+
+    %exitcondition needs to be checked
+
+    lambda = v'*(A*v);
+    x = v;
+end
