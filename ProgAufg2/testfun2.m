da0adb0660b12e05afda42ee05cdace45d11c6d8
Felix_Desktop <dede6487@gmail.com>
2022-12-19 11:47:36 +0100

    testfuns done

--- ProgAufg2/testfun2.m
+++ ProgAufg2/testfun2.m
@@ -1,2 +1,8 @@
 function [y] = testfun2 (x)
-  y = sin(x);
+%takes a vector of x and gives a vector y with the corresponding function values
+
+y = zeros(length(x),1);
+
+for i = 1:length(x)
+    y(i) = sin(x(i));
+end
--- ProgAufg2/testfun3.m
+++ ProgAufg2/testfun3.m
@@ -1,2 +1,8 @@
 function [y] = testfun3 (x)
-  y = exp(x) - x*x/2;
+%takes a vector of x and gives a vector y with the corresponding function values
+
+y = zeros(length(x),1);
+
+for i = 1:length(x)
+    y(i) = exp(x(i)) - x(i)*x(i)/2;
+end  
